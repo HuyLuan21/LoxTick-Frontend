32443bf92fa61d75f5240e02b78b562ab67a6411
HuyLuan21 <nguyenluan03122005@gmail.com>
2026-03-17 15:28:06 +0700

    fix:build

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 // import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import { TooltipProvider } from "./components/ui/tooltip";
